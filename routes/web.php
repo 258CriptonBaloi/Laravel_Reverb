67f29d0060c86b0a8fb156f29e1d4f9356d73169
Cripton Baloi <criptonbaloi9@gmail.com>
2024-10-06 19:22:31 +0200

    sending events

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Events\Example;
+use App\Events\OrderDispatched;
+use App\Http\Controllers\ProfileController;
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/broadcast', function () {
+    broadcast(new OrderDispatched(User::find(1)));
+});
+
+/* Route::get('/broadcast', function () {
+    broadcast(new Example(User::find(1), Message::find(1)));
+}); */
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
